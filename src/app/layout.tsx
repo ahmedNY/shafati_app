366c3619ea5f22f30ebe66bd95fe1e02a2db855e
Ahmed Nabil <ahmed.n.yagoub@gmail.com>
2025-10-24 00:06:46 +0300

    remove home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,16 @@
 import "@/styles/globals.css";
 
-import { type Metadata } from "next";
+// import { type Metadata } from "next";
 import { Tajawal } from "next/font/google";
 
 import { TRPCReactProvider } from "@/trpc/react";
 import { SessionProvider } from "next-auth/react";
 
-export const metadata: Metadata = {
-  title: "مشروع قاسيون | رؤية جديدة للسياحة",
-  description: "منصة متكاملة لإدارة وتطوير المشاريع السياحية في جبل قاسيون.",
-  icons: [{ rel: "icon", url: "/favicon.ico" }],
-};
+// export const metadata: Metadata = {
+//   title: "مشروع قاسيون | رؤية جديدة للسياحة",
+//   description: "منصة متكاملة لإدارة وتطوير المشاريع السياحية في جبل قاسيون.",
+//   icons: [{ rel: "icon", url: "/favicon.ico" }],
+// };
 
 // Configure the Arabic font
 const tajawal = Tajawal({
